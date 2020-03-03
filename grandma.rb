a1fac8bc2c9402734f4616d1d0e98eb7a9ea2691
Brittany Russell <sincerelybr@gmail.com>
2020-03-03 23:53:01 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,5 +17,7 @@ def speak_to_grandma(phrase)
     puts "I LOVE YOU TOO PUMPKIN"
   elsif phrase == phrase.upcase
     puts "NO, NOT SINCE 1938!" 
+  else phrase == phrase.downcase
+    puts 
   end
 end
